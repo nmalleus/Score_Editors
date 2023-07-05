2e35e9054b41fc5f41cb46031d32f280ec0f2188
nmalleus <nelson@lamiprod.fr>
2023-07-05 10:55:58 +0200

    Start working on Macro multiple flows print

---
+++ Dorico/Files/Scripts WIP/Multi_flow_exports.lua
@@ -0,0 +1,6 @@
+local app=DoApp.DoApp()
+
+-- the choice between "Printer" and "Graphics" does not seem possible through macro, we goth these same two lines in any cases:
+-- app:doCommand([[Print.RemovePrintLayoutRendererInternal]])
+-- app:doCommand([[Print.AddPrintLayoutRendererInternal]])
+
