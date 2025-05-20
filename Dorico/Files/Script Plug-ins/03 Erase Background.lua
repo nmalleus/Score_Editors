50b19fe6782266385fe7388fdd55ad16ff85b3e9
nmalleus <nelson@lamiprod.fr>
2025-05-20 11:49:34 +0200

    Templates and erase background upgrade

--- Dorico/Files/Script Plug-ins/03 Erase Background.lua
+++ Dorico/Files/Script Plug-ins/03 Erase Background.lua
@@ -1,4 +1,6 @@
 local app=DoApp.DoApp()
 app:doCommand([[Window.SwitchMode?WindowMode=kEngraveMode]])
 app:doCommand([[UI.InvokePropertyChangeValue?Type=kTextErasure&Value=null: ]])
+app:doCommand([[UI.InvokePropertyChangeValue?Type=kPlayingTechniqueErasure&Value=null: ]])
+app:doCommand([[UI.InvokePropertyChangeValue?Type=kDynamicErasure&Value=null: ]])
 app:doCommand([[Window.SwitchMode?WindowMode=kWriteMode]])
--- Dorico/Files/Script Plug-ins/04 Erase Background Dyn.lua
+++
@@ -1,4 +0,0 @@
-local app=DoApp.DoApp()
-app:doCommand([[Window.SwitchMode?WindowMode=kEngraveMode]])
-app:doCommand([[UI.InvokePropertyChangeValue?Type=kDynamicErasure&Value=null: ]])
-app:doCommand([[Window.SwitchMode?WindowMode=kWriteMode]])
