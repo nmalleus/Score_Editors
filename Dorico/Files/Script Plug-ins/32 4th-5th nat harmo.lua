b5692d69a0eceb86a7e4e27a48ce29ab3840b57d
nmalleus <nelson@lamiprod.fr>
2026-08-17 12:09:35 +0200

    Back

---
+++ Dorico/Files/Script Plug-ins/32 4th-5th nat harmo.lua
@@ -0,0 +1,4 @@
+local app=DoApp.DoApp()
+app:doCommand([[UI.InvokePropertyChangeValue?Type=kNoteHarmonicType&Value=string: "kNatural"]])
+app:doCommand([[UI.InvokePropertyChangeValue?Type=kNoteNaturalHarmonicStyle&Value=string: "kWhiteDiamondNotehead"]])
+app:doCommand([[UI.InvokePropertyChangeValue?Type=kNoteNaturalHarmonicShowOpenString&Value=string: "true"]])
